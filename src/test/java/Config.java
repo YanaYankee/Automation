345047b4d7175842c67619670671d88e56176bc7
Yanina <yanina.pavlyk@gmail.com>
2019-03-17 16:14:42 +0200

    Pet-store project: first tests

---
+++ src/test/java/Config.java
@@ -0,0 +1,7 @@
+public class Config {
+    //public static (the only one )  final =>constant (uppercase + _ , other variables camelCase)
+    public static final String PET_STORE_BASE_HOST = "https://petstore.swagger.io/v2";
+
+    public static final String GET_PET_ENDPOINT = "/pet/{id}"; //instead of 0 another
+
+}
---
+++ src/test/java/PetStoreTests.java
@@ -0,0 +1,74 @@
+import io.restassured.RestAssured;
+//import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class PetStoreTests {
+
+
+    @Test
+    public void getPetById200 () {
+      //  baseSpecification()
+        given() //renamed from baseSpecification
+                .basePath(Config.GET_PET_ENDPOINT)
+                .pathParam("id",2)
+                .get()
+                .then().statusCode(200);
+    }
+
+  //  private RequestSpecification baseSpecification() { //setup base config
+  private RequestSpecification given() { //renamed from baseSpecification
+        return  RestAssured.given()
+                .baseUri(Config.PET_STORE_BASE_HOST)
+                .log().uri(); // log request
+    }
+
+
+
+ //3 part
+//
+//    @Test
+//    public void getPetById200 () {
+//        RestAssured.
+//                given()
+//                .baseUri(Config.PET_STORE_BASE_HOST)
+//        //        .basePath("/pet/1")
+//                .basePath(Config.GET_PET_ENDPOINT)
+//                .pathParam("id",2)
+//              //  .baseUri("https://petstore.swagger.io/v2/pet/t")
+//                .log().uri() // log request
+//                .get()
+//                .then().statusCode(200);
+//    }
+
+ //1-2 part
+
+//    @Test
+//    public void getPetById200 () {
+//        //send request to get json
+////        Response response = (Response) RestAssured.
+////                given()
+////                .baseUri("https://petstore.swagger.io/v2/pet/1")
+////                .get()
+////                .then().statusCode(200)
+////                .and().extract().body();
+////        System.out.println(response);
+//
+//        RestAssured.
+//                given()
+//                .baseUri("https://petstore.swagger.io/v2/pet/1")
+//                .get()
+//                .then().statusCode(200);
+//    }
+//
+//    @Test
+//    public void getPetById404 () {
+//
+//
+//        RestAssured.
+//                given()
+//                .baseUri("https://petstore.swagger.io/v2/pet/t")
+//                .get()
+//                .then().statusCode(404);
+//    }
+}
